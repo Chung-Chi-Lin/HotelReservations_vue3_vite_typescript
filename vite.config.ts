2949460214bf5e655c271d3d235fe428d4f61a6d
ELAND\zhongqilin <z0925955648@gmail.com>
2024-02-06 17:23:50 +0800

    fix action

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 import { fileURLToPath, URL } from "node:url";
-
+// base: process.env.NODE_ENV === "production" ? "/HotelReservations_vue3_vite_typescript/" : "/",
 export default defineConfig({
   plugins: [
     vue(),
@@ -10,7 +10,7 @@ export default defineConfig({
       include: ["src/**/*.ts", "src/**/*.vue", "src/*.ts", "src/*.vue"],
     })
   ],
-  base: process.env.NODE_ENV === "production" ? "/HotelReservations_vue3_vite_typescript/" : "/",
+  base: "/HotelReservations_vue3_vite_typescript/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
